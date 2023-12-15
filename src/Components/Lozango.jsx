665327a68d4934bcfa4d7a1f8f93e8b460269ab1
vilardo22 <gabrielvilardo22@gmail.com>
2023-12-14 23:05:22 -0300

    Lozangos

--- src/Components/Lozango.jsx
+++ src/Components/Lozango.jsx
@@ -3,17 +3,11 @@ import styles from "./LOzango.module.css";
 const Lozango = () => {
   return (
     <div class={styles.card}>
-        <div className={styles.border}></div>
-        
-    <div class={styles.content}>
-      <div class={styles.logo}>
-        <div></div>
-        <div></div>
+      <div className={styles.border}></div>
+
+      <div class={styles.content}>
       </div>
-     
-     
     </div>
-  </div>
   )
 }
 
--- src/Components/Slideshow.jsx
+++ src/Components/Slideshow.jsx
@@ -6,33 +6,33 @@ import Lozango from './Lozango';
 
 const Slider = () => {
   return (
-    <>
-      <div className={styles.container}>
-        {/* <div className={styles.textContainer}>
+
+    <div className={styles.container}>
+      {/* <div className={styles.textContainer}>
           <p className={styles.Text}>Quais lugares você vai visitar</p>
           <h1 className={styles.textInH1}>Loren Ipsum</h1>
         </div> */}
-      </div>
-      <div className={styles.container2}>
-        {/* <div className={styles.backgroundImage} style={{ backgroundImage: `url(${f1})` }}></div> */}
-        {/* <div className={styles.textContainer2}>
+
+
+      {/* <div className={styles.backgroundImage} style={{ backgroundImage: `url(${f1})` }}></div> */}
+      {/* <div className={styles.textContainer2}>
           <h1 className={styles.textInH12}>Loren Ipsum</h1>
           <p className={styles.Text2}>Quais lugares você vai visitar</p>
         </div> */}
-        <div className={styles.card1}>
-          {/* <Lozango /> */}
-          <div className={styles.bottomText}>Texto na parte inferior</div>
-        </div>
-        <div className={styles.card2}>
-          {/* <Lozango /> */}
-          <div className={styles.bottomText}>Texto na parte inferior</div>
-        </div>
-        <div className={styles.card3}>
-          {/* <Lozango /> */}
-          <div className={`${styles.bottomText} ${styles.cardTop}`}>Texto na parte superior</div>
-        </div>
+      <div className={styles.bottomText2}>Texto1 na parte inferior</div>
+
+      <div className={styles.card1}>
       </div>
-    </>
+      <div className={styles.bottomText1}>Texto1 na parte inferior</div>
+
+      <div className={styles.card2}>
+      </div>
+      <div className={styles.card3}>
+      </div>
+      <div className={styles.bottomText3}>Texto1 na parte inferior</div>
+
+
+    </div>
   );
 };
 
